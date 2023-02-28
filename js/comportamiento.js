844297d2d903958055e500ca970e1a822368150f
carlosgvelasquez <carlosvelasquezmenchaca@frba.utn.edu.ar>
2023-02-28 15:36:32 -0300

    Me falto ir commiteando poco a poco, Entrega Final

---
+++ js/comportamiento.js
@@ -0,0 +1,23 @@
+ function calcularEdad() {
+    var hoy = new Date();
+    var cumpleanos = new Date(1996, 8,9);
+    var edad = hoy.getFullYear() - cumpleanos.getFullYear();
+    var m = hoy.getMonth() - cumpleanos.getMonth();
+
+    if (m < 0 || (m === 0 && hoy.getDate() < cumpleanos.getDate())) {
+        edad--;
+    }
+
+    document.getElementById("birthday").innerHTML = edad;
+}
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;
+}
